41ef9f81a5048143b7b6564f9f3f19814b753f66
Mark Jabel Frias <friasmarkjabel@gmail.com>
2022-02-18 22:29:38 +0800

    Add rate limiting

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const app = express();
 const userControllers = require('../controllers/users.controller');
 const router = express.Router();
 const { authorize } = require('../auth/authorization');
+
+
 
 router.get("/", authorize, userControllers.getUsers);
 router.post("/register", userControllers.registerUser);
