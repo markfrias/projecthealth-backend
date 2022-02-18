c3ac32870825fd51b46e7900d2e1896cd49a5130
Mark Jabel Frias <friasmarkjabel@gmail.com>
2022-02-18 22:29:38 +0800

    Add rate limiting

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 const mysql = require('mysql');
 const { connection } = require('./dbConfig');
 const usersRouter = require('./routes/users.route');
+const rateLimit = require('express-rate-limit')
+
 
 const app = express();
+
 
 // Middleware
 app.use(cors());
@@ -43,6 +46,21 @@ connection.query('SELECT * FROM Users', (err, results, fields) => {
 
 })
 */
+
+
+
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use('/api', limiter)
+
+
 
 // Routes
 app.use('/api/users', usersRouter);
