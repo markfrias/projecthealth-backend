50d72facdca2ee88b8f51882d151a0bf0298d1bc
Mark Jabel Frias <friasmarkjabel@gmail.com>
2022-02-25 08:48:22 +0800

    Don't require photos url from quick note entry

--- controllers/food.controller.js
+++ controllers/food.controller.js
@@ -2,18 +2,17 @@ const { connection } = require('../dbConfig');
 
 // Create a new food journal entry
 const createFoodEntry = (req, res) => {
-    const { foodJournalId, foodJournalDate, mealType, diaryType, foodId, foodName, servingDescription, servingUnit, servingQty, caloriesPerUnit, caloriesPer100g, carbs, protein, fat, sodium, weightInG, photosUrl } = req.body;
+    const { mealType, diaryType, foodId, foodName, servingDescription, servingUnit, servingQty, caloriesPerUnit, caloriesPer100g, carbs, protein, fat, sodium, weightInG, photosUrl } = req.body;
     const userId = req.body.userId;
     // Create food entry for quick note food diary entry
     try {
         if (diaryType === "quick") {
 
             // Check for undefined fields
-            if (!userId || !mealType || !foodName || !servingDescription || !photosUrl) {
+            if (!userId || !mealType || !foodName || !servingDescription) {
                 res.status(400);
                 res.json({ message: "Some fields are not filled" });
             } else {
-                console.log(userId, mealType, foodName, servingDescription, photosUrl)
 
                 // Save quick note to food journal
                 connection.query('INSERT INTO FoodJournal(userId, foodJournalDate, mealType, diaryType, foodName, servingDescription, photosUrl) VALUES (?, ?, ?, ?, ?, ?, ?)', [userId, new Date(), mealType, diaryType, foodName, servingDescription, photosUrl], (error, results, fields) => {
@@ -35,8 +34,9 @@ const createFoodEntry = (req, res) => {
                 // Save detailed log to food journal
                 connection.query('INSERT INTO FoodJournal(userId, foodJournalDate, mealType, diaryType, foodId, foodName, servingUnit, servingQty, caloriesPerUnit, caloriesPer100g, carbs, protein, fat, sodium, weightInG) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [userId, new Date(), mealType, diaryType, foodId, foodName, servingUnit, servingQty, caloriesPerUnit, caloriesPer100g, carbs, protein, fat, sodium, weightInG], (error, results, fields) => {
                     if (error) {
-                        console.log(error)
-                        res.json({ message: "An error ffs" })
+                        //console.log(error)
+                        res.status(500);
+                        res.json({ message: "Server error" })
                     }
 
                     res.json(results);
@@ -49,6 +49,8 @@ const createFoodEntry = (req, res) => {
 
     } catch (error) {
         console.log("Probably a TypeError");
+        res.status(500);
+        res.json({ message: "Data input erroneous" })
     }
 }
 
